70cfc2b43a8be49962d644191e0781533bedca4a
Doug Wright <mrdougwright@gmail.com>
2013-12-14 19:42:21 -0800

    First Commit. Angular added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restauranteur::Application.config.secret_key_base = 'c1cebf645ca28287bad9baa474a9a1af3f57b66d3da02cc9ef0205ea4a991d87b1d7a876345a44b96deebe1fc78e73fbf77270837a1a49aa6ee0dd184923efcb'
